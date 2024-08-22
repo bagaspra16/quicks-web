4485aee957e4afd185f7c6bb9c1d4df27196f162
bagaspra16 <bagaspratamajunianika72@gmail.com>
2024-08-22 18:16:36 +0700

    Update: beautify Russel chat

--- js/script.js
+++ js/script.js
@@ -735,6 +735,7 @@
                     .replace(/\n/g, '')
                     .replace(/\t/g, '')
                     .replace(/\n\n/g, '')
+                    .replace(/nn/g, '')
                     .replace(/\\/g, ''); 
             
                 messageElement.innerHTML = formattedMessage;
